4dfb3df7fb145c6127facdb9fa57b7e8700816ed
RavishRS4 <ravishsingh807@gmail.com>
2025-08-07 13:26:54 +0530

    Add files via upload

---
+++ anudip.sql
@@ -0,0 +1,40 @@
+create database Employee_Management_System;
+
+use  Employee_Management_System;
+
+
+//1.Create a table called Employees with the following columns:
+
+EmployeeID (INTEGER, Primary Key)
+
+FirstName (VARCHAR(50))
+
+LastName (VARCHAR(50))
+
+Salary (DECIMAL(10,2))  //
+desc Employee;
+
+Query:
+
+
+
+
+
+
+//2.Insert  5 employees records.
+
+
+Query:
+
+
+
+//3.Alter the table to include a new column.:Add a new column Departments (VARCHAR(50)) to the table.
+
+Query:
+
+
+
+//4.Get the names and department of employees who earn more than 55000.
+
+
+Query:
